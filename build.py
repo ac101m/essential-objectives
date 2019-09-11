c68272239e2936db2f9a9bae9becc31e9be4c2b1
ac101m <mofty101@gmail.com>
2019-09-11 01:04:53 +0100

    Fix ignored directory structure

--- build.py
+++ build.py
@@ -7,7 +7,8 @@ src_dir = 'src'
 
 zip_file = zipfile.ZipFile('essential-objectives.zip', 'w')
 with zip_file:
-    for root, directories, files in os.walk(src_dir):
+    for root, dirs, files in os.walk(src_dir):
+        dir = root[len(src_dir):]
         for file in files:
-            print('Compressing: ' + os.path.join(root, file))
-            zip_file.write(os.path.join(root, file), file)
+            print('Zipping: ' + os.path.join(root, file))
+            zip_file.write(os.path.join(root, file), os.path.join(dir, file))
